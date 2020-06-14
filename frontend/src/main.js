9320dc78190dbb24919f954150af9a11d3d1b213
Brice Friha <brice.friha@aol.fr>
2020-06-14 17:42:56 +0200

    set up bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,41 @@
 import Vue from 'vue'
 import App from './App.vue';
 import VueRouter from 'vue-router';
+import PortalVue from 'portal-vue';
+// import PopperJs from '@popperjs/core';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+// This imports all the layout components such as <b-container>, <b-row>, <b-col>:
+import { LayoutPlugin } from 'bootstrap-vue'
+Vue.use(LayoutPlugin)
+
+// This imports <b-modal> as well as the v-b-modal directive as a plugin:
+import { ModalPlugin } from 'bootstrap-vue'
+Vue.use(ModalPlugin)
+
+// This imports <b-card> along with all the <b-card-*> sub-components as a plugin:
+import { CardPlugin } from 'bootstrap-vue'
+Vue.use(CardPlugin)
+
+// This imports directive v-b-scrollspy as a plugin:
+import { VBScrollspyPlugin } from 'bootstrap-vue'
+Vue.use(VBScrollspyPlugin)
+
+// This imports the dropdown and table plugins
+import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
+Vue.use(DropdownPlugin)
+Vue.use(TablePlugin)
 
 // Integrate vue-router to the app 
 Vue.use(VueRouter);
+
+// Integrate portal to the app 
+Vue.use(PortalVue);
+
+// Integrate PopperJs to the app 
+// Vue.use(PopperJs);
 
 Vue.config.productionTip = false;
 
