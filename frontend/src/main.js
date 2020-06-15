b77f69ca35d4a433969c120af1b91cf12e4273b8
Brice Friha <brice.friha@aol.fr>
2020-06-15 16:48:16 +0200

    Add vuex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 
 import PortalVue from 'portal-vue';
 import router from './router';
+import store from './store';
 import VueRouter from 'vue-router';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -47,4 +48,5 @@ Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
   router,
+  store,
 }).$mount('#app')
