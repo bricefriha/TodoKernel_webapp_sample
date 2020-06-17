a707ccbf0dce70328d14dc373299d1bac8acca60
Brice Friha <brice.friha@aol.fr>
2020-06-17 10:47:12 +0200

    I created dasboad route

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,19 +1,35 @@
 
 import HelloWorld from './components/HelloWorld.vue';
 import Login from './components/Login.vue';
+import Dashboard from './components/Dashboard.vue';
 import VueRouter from 'vue-router';
+// ToDo: importing store seems totally dumb to me but it works for now  
+import store from './store';
+
 
 
 const routes = [
     { path: '/', name: 'home', component: HelloWorld},
     { path: '/login', name: 'login', component: Login},
     { path: '/signup', name: 'signup', component: Login},
+    { path: '/dashboard', name: 'dashboard', component: Dashboard, meta: { private: true }},
 ];
 
 const router = new VueRouter({
     mode: 'history',
     name: 'Router',
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+
+    // if the route is set as private and the user is not connected
+    if (to.meta.private && !store.state.user) {
+        // rederect to login
+        next({name: 'login'});
+        return;
+    }
+    next();
 });
 
 export default router;
